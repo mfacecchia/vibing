574e91c2de19e709adfffa6cd20ad6a8493be682
Marco Facecchia <facecchiamarco@icloud.com>
2025-06-14 00:01:19 +0200

    feat: initial auth CLI args declaration

---
+++ vibing/src/cli/args.rs
@@ -0,0 +1,29 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[clap(author, version)]
+pub struct VibingCliParser {
+    #[command(subcommand)]
+    pub command: BaseCommands,
+
+    /// Print out the trace from all the methods called
+    #[arg(short, long)]
+    pub verbose: bool
+}
+
+#[derive(Subcommand, Debug)]
+pub enum BaseCommands {
+    /// Authenticate to the application (required for most of the operations)
+    #[command(subcommand)]
+    Auth(AuthArgs),
+}
+
+#[derive(Subcommand, Debug)]
+pub enum AuthArgs {
+    /// Initiate the login process
+    Login,
+    /// Logout from the current logger in account
+    Logout,
+    /// Check your current authentication status
+    Check
+}
