acad3bc57b883d37bf28c8a57c4c90f9d833b017
Marco Facecchia <facecchiamarco@icloud.com>
2025-06-14 04:15:09 +0200

    feat: add credential removal function

--- vibing/src/creds.rs
+++ vibing/src/creds.rs
@@ -2,6 +2,7 @@ use keyring::Entry;
 
 use crate::error::Result;
 
+// TODO: Get serivce from function argument
 pub fn store_cred(user: &str, password: &str) -> Result<()> {
     let entry = Entry::new("vibing", user)?;
     entry.set_password(password).unwrap();
@@ -12,4 +13,10 @@ pub fn store_cred(user: &str, password: &str) -> Result<()> {
 pub fn get_cred(service: &str, user: &str) -> Result<String> {
     let entry = Entry::new(service, user)?;
     Ok(entry.get_password().unwrap())
+}
+
+pub fn remove_cred(service: &str, user: &str) -> Result<()> {
+    let entry = Entry::new(service, user)?;
+    entry.delete_credential()?;
+    Ok(())
 }
