3404385ea91774da86260830d44661ad4ba6c90b
Marco Facecchia <facecchiamarco@icloud.com>
2025-06-14 02:33:16 +0200

    feat: add login method & update matcher function return type

--- vibing/src/cli/matcher.rs
+++ vibing/src/cli/matcher.rs
@@ -1,21 +1,35 @@
+use crate::{
+    authentication::keycloak_auth::KeycloakRequest,
+    cli::args::{AuthArgs, BaseCommands, VibingCliParser},
+    env,
+    error::Result,
+};
 use clap::Parser;
-use crate::{authentication::auth::KeycloakRequest, cli::args::{AuthArgs, BaseCommands, VibingCliParser}, env};
 
-pub async fn handle_cli_args() -> () {
+pub async fn handle_cli_args() -> Result<()> {
     let args: VibingCliParser = VibingCliParser::parse();
     let verbose_flag = args.verbose;
 
     match args.command {
-        BaseCommands::Auth(auth_commands) => match_auth_args(auth_commands, verbose_flag).await,
+        BaseCommands::Auth(auth_commands) => match_auth_args(auth_commands, verbose_flag).await?,
     }
+    Ok(())
 }
 
-async fn match_auth_args (command: AuthArgs, verbose: bool) -> () {
+async fn match_auth_args(command: AuthArgs, verbose: bool) -> Result<()> {
+    let auth_request = KeycloakRequest::new();
     match command {
         AuthArgs::Login => {
-            todo!();
+            auth_request
+                .authenticate(
+                    env::get_env("VIBING_DEVICE_CODE_CLIENT_ID").as_str(),
+                    env::get_env("VIBING_CLIENT_SECRET").as_str(),
+                    verbose,
+                )
+                .await?;
         }
         AuthArgs::Logout => todo!(),
         AuthArgs::Check => todo!(),
     }
+    Ok(())
 }
