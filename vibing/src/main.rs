2d76d0dcbafa4d64eeb88323d8f247e027519f60
Marco Facecchia <facecchiamarco@icloud.com>
2025-06-17 23:12:38 +0200

    feat: add error handling in main app

--- vibing/src/main.rs
+++ vibing/src/main.rs
@@ -1,8 +1,16 @@
-use vibing::{cli::{matcher}, env};
+use vibing::{cli::matcher, env, error::handle};
 
 #[tokio::main]
 async fn main() {
     println!("Vibing ~");
     env::load();
-    matcher::handle_cli_args().await;
+    // FIXME: Still panicks ig user cancels the credentials read operation
+    let result = matcher::handle_cli_args().await;
+    if let Err(result_err) = &result {
+        println!("Error thrown");
+        println!("{result_err:#?}");
+    }
+    if let Err(err) = result {
+        handle(err);
+    }
 }
