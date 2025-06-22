b8bf8a1aaca0adeff4483e4704a214d3f70ac2fe
Marco Facecchia <facecchiamarco@icloud.com>
2025-06-22 12:35:06 +0200

    feat: add utility function for authentication token obtaining from secure storgae

--- vibing/src/creds.rs
+++ vibing/src/creds.rs
@@ -1,6 +1,6 @@
 use keyring::Entry;
 
-use crate::error::Result;
+use crate::{env, error::Result};
 
 // TODO: Get serivce from function argument
 pub fn store_cred(user: &str, password: &str) -> Result<()> {
@@ -12,7 +12,14 @@ pub fn store_cred(user: &str, password: &str) -> Result<()> {
 
 pub fn get_cred(service: &str, user: &str) -> Result<String> {
     let entry = Entry::new(service, user)?;
-    Ok(entry.get_password().unwrap())
+    Ok(entry.get_password()?)
+}
+
+/// Uses default service and user to obtain authentication token from secure storage
+pub fn get_cred_use_defaults() -> Result<String> {
+    let service = env::get_env("CREDENTIAL_STORE_AUTH_SERVICE");
+    let user = env::get_env("CREDENTIAL_STORE_AUTH_USER");
+    get_cred(service.as_str(), user.as_str())
 }
 
 pub fn remove_cred(service: &str, user: &str) -> Result<()> {
